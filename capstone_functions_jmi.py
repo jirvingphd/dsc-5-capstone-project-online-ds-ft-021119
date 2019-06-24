1cecd9ea5ba467dfbc4130fa6c99865e4de9f87d
James M. Irving <james.irving.phd@outlook.com>
2019-06-24 16:46:58 -0400

    added functions for keras-prep

--- capstone_functions_jmi.py
+++ capstone_functions_jmi.py
@@ -5,3 +5,37 @@ import matplotlib as mpl
 
 def merge_twitter_stock_dfs_on_date(stocks_df, twitter_df):
     pass
+
+
+def stationarity_check(df, col):
+    TS = df
+    # Import adfuller
+    from statsmodels.tsa.stattools import adfuller
+    import pandas as pd
+    import numpy as np
+    
+    # Calculate rolling statistics
+    rolmean = TS.rolling(window = 8, center = False).mean()
+    rolstd = TS.rolling(window = 8, center = False).std()
+    
+    # Perform the Dickey Fuller Test
+    dftest = adfuller(TS['col']) # change the passengers column as required 
+    
+    #Plot rolling statistics:
+    fig = plt.figure(figsize=(12,6))
+    orig = plt.plot(TS, color='blue',label='Original')
+    mean = plt.plot(rolmean, color='red', label='Rolling Mean')
+    std = plt.plot(rolstd, color='black', label = 'Rolling Std')
+    plt.legend(loc='best')
+    plt.title('Rolling Mean & Standard Deviation')
+    plt.show(block=False)
+    
+    # Print Dickey-Fuller test results
+    print ('Results of Dickey-Fuller Test:')
+
+    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+    for key,value in dftest[4].items():
+        dfoutput['Critical Value (%s)'%key] = value
+    print (dfoutput)
+    
+    return None
