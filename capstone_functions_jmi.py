5e7714a59f0deef48e9fa0790f2a43874684e49c
James M. Irving <james.irving.phd@outlook.com>
2019-06-28 02:07:02 -0400

    added colab notebook

--- capstone_functions_jmi.py
+++ capstone_functions_jmi.py
@@ -1,37 +1,211 @@
+
+## SAVE AND LOAD STOCK MARKET MINUTE DATA
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
-def merge_twitter_stock_dfs_on_date(stocks_df, twitter_df):
-    pass
+##################### FILE & VAR MANAGEMENT FUNCTIONS #####################
+
+def mount_google_drive(force_remount=True):
+    from google.colab import drive
+    print('drive_filepath="drive/My Drive/"')
+    return drive.mount('/content/drive', force_remount=force_remount)
 
 
-def stationarity_check(df, col):
-    TS = df
+def check_for_google_drive(mount_if_not=True):
+    import os
+    # Check if google drive is already mounted
+    x=[]
+    try: os.listdir('drive/My Drive/')
+    except FileNotFoundError: x=None
+
+    # if GDrive not mounted, mount it
+    if x==None:
+        print('Drive not mounted.')
+        if mount_if_not==False:
+            return False
+        else:
+            print('Mounting google drive...')
+            return mount_google_drive()
+    else:
+        return True
+
+# def cd_project_folder(folder_path ='/content/drive/My Drive/Colab Notebooks/Mod 5 Project/data/'):
+#     import os
+#     return os.chdir(folder_path),print('Cur Dir:', os.getcwd())
+#     # print('Dir Contents:\n',os.listdir())
+    
+    
+#################### GENERAL HELPER FUNCTIONS #####################
+def is_var(name):
+    x=[]
+    try: eval(name)
+    except NameError: x = None
+        
+    if x is None:
+        return False
+    else:
+        return True      
+    
+#################### TIMEINDEX FUNCTIONS #####################
+def get_day_window_size_from_freq(dataset):
+    if dataset.index.freq=='T':
+        day_window_size = 1440
+    elif dataset.index.freq=='BH':
+        day_window_size = 8
+    elif dataset.index.freq=='BD':
+        day_window_size=1
+    elif dataset.index.freq=='D':
+        day_window_size=1
+        
+    else:
+        raise Exception('dataset freq=None')
+        
+    return day_window_size
+
+    
+def  set_timeindex_freq(ive_df, col_to_fill=None, freq='T',fill_method='ffill',
+                        verbose=3): #set_tz=True,
+    
+    import pandas as pd
+    import numpy as np
+    
+    
+    if verbose>1:
+        # print(f"{'Index When:':>{10}}\t{'Freq:':>{20}}\t{'Index Start:':>{40}}\t{'Index End:':>{40}}")
+        print(f"{'Index When:'}\t{'Freq:'}\t{'Index Start'}\t\t{'Index End:'}")
+        print(f"Pre-Change\t{ive_df.index.freq}\t{ive_df.index[0]}\t{ive_df.index[-1]}")
+        
+    # Change frequency to frewq
+    ive_df = ive_df.asfreq(freq)#'min')
+    
+    #     # Set timezone
+    #     if set_tz==True:
+    #         ive_df.tz_localize()
+    #         ive_df.index = ive_df.index.tz_convert('America/New_York')
+    
+    # Report Success / Details
+    if verbose>1:
+        print(f"Post-Change\t{ive_df.index.freq}\t{ive_df.index[0]}\t{ive_df.index[-1]}")
+
+
+    ## FILL AND TRACK TIMEPOINTS WITH MISSING DATA    
+    
+    # Helper Function for adding column to track the datapoints that were filled
+    def check_null_times(x):
+        import numpy as np
+        if np.isnan(x):
+            return True
+        else:
+            return False
+
+    ## CREATE A COLUMN TO TRACK ROWS TO BE FILLED
+    # If col_to_fill provided, use that column to create/judge ive_df['filled_timebin'] 
+    if col_to_fill!=None:
+        ive_df['filled_timebin'] = ive_df[col_to_fill].apply(lambda x: check_null_times(x))#True if ive_df.isna().any()
+        
+    # if not provided, use all columns and sum results
+    elif col_to_fill == None:
+        # Prefill fol with 0's
+        ive_df['filled_timebin']=0
+        
+        # loop through all columns and add results of check_null_times from each loop
+        for col in ive_df.columns:
+            
+            #ive_df['filled_timebin'] = ive_df[target_col].apply(lambda x: check_null_times(x))#True if ive_df.isna().any()
+            curr_filled_timebin_col = ive_df[col].apply(lambda x: check_null_times(x))#True if ive_df.isna().any() 
+            
+            # add results
+            ive_df['filled_timebin'] +=  curr_filled_timebin_col
+
+            
+    ## FILL IN NULL VALUES
+    ive_df.fillna(method=fill_method, inplace=True)
+
+    # Report # filled
+    if verbose>0:
+        check_fill = ive_df.loc[ive_df['filled_timebin']>0]
+        print(f'\nFilled {len(check_fill==True)}# of rows using method {fill_method}')
+    
+    # Report any remaning null values
+    if verbose>0:
+        res = ive_df.isna().sum()
+        if res.any():
+            print(f'Cols with Nulls:')
+            print(res[res>0])
+        else:
+            print('No Remaining Null Values')   
+            
+    # display header
+    if verbose>2:
+        display(ive_df.head())
+    
+    return ive_df
+
+
+############### TIMESERIES TESTS AND VISUALS ###############
+
+def plot_time_series(stocks_df, freq=None, fill_method='ffill',figsize=(12,4)):
+    
+    df = stocks_df.copy()
+    df.fillna(method=fill_method, inplace=True)
+    df.dropna(inplace=True)
+    
+    if (df.index.freq==None) & (freq == None):
+        xlabels=f'Time'
+    
+    elif (df.index.freq==None) & (freq != None):
+        df = df.asfreq(freq)
+        df.fillna(method=fill_method, inplace=True)
+        df.dropna(inplace=True)
+        xlabels=f'Time - Frequency = {freq}'
+
+    else:
+        xlabels=f'Time - Frequency = {df.index.freq}'
+        
+    ylabels="Price"
+
+    raw_plot = df.plot(figsize=figsize)
+    raw_plot.set_title('Stock Bid Closing Price ')
+    raw_plot.set_ylabel(ylabels)
+    raw_plot.set_xlabel(xlabels)
+    
+    
+def stationarity_check(df, col='BidClose', window=80, freq='BH'):
+    """From learn.co lesson: use ADFuller Test for Stationary and Plot"""
+    import matplotlib.pyplot as plt
+    TS = df[col].copy()
+    TS = TS.asfreq(freq)
+    TS.fillna(method='ffill',inplace=True)
+    TS.dropna(inplace=True)
     # Import adfuller
     from statsmodels.tsa.stattools import adfuller
     import pandas as pd
     import numpy as np
     
     # Calculate rolling statistics
-    rolmean = TS.rolling(window = 8, center = False).mean()
-    rolstd = TS.rolling(window = 8, center = False).std()
+    rolmean = TS.rolling(window = window, center = False).mean()
+    rolstd = TS.rolling(window = window, center = False).std()
     
     # Perform the Dickey Fuller Test
-    dftest = adfuller(TS['col']) # change the passengers column as required 
+    dftest = adfuller(TS) # change the passengers column as required 
     
     #Plot rolling statistics:
-    fig = plt.figure(figsize=(12,6))
-    orig = plt.plot(TS, color='blue',label='Original')
-    mean = plt.plot(rolmean, color='red', label='Rolling Mean')
-    std = plt.plot(rolstd, color='black', label = 'Rolling Std')
-    plt.legend(loc='best')
-    plt.title('Rolling Mean & Standard Deviation')
-    plt.show(block=False)
+    fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(8,4))
+    ax[0].set_title('Rolling Mean & Standard Deviation')
+
+    ax[0].plot(TS, color='blue',label='Original')
+    ax[0].plot(rolmean, color='red', label='Rolling Mean',alpha =0.6)
+    ax[1].plot(rolstd, color='black', label = 'Rolling Std')
+    ax[0].legend()
+    ax[1].legend()
+#     plt.show(block=False)
+    plt.tight_layout()
     
     # Print Dickey-Fuller test results
     print ('Results of Dickey-Fuller Test:')
+    print('\tIf p<.05 then timeseries IS stationary.')
 
     dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
     for key,value in dftest[4].items():
@@ -39,3 +213,143 @@ def stationarity_check(df, col):
     print (dfoutput)
     
     return None
+
+##################### DATASET LOADING FUNCTIONS #####################   
+def load_raw_stock_data_from_txt(filename='IVE_bidask1min.txt', 
+                               folderpath='/content/drive/My Drive/Colab Notebooks/Mod 5 Project/data/',
+                               verbose=2):
+    
+    check_for_google_drive()
+    
+    import pandas as pd
+    
+    headers = ['Date','Time','BidOpen','BidHigh','BidLow','BidClose','AskOpen','AskHigh','AskLow','AskClose']
+    
+    fullfilename= folderpath+filename
+    ive_df = pd.read_csv(fullfilename, names=headers,parse_dates=True)
+    
+    # Create datetime index
+    date_time_index = ive_df['Date']+' '+ive_df['Time']
+    
+    #ive_df.index=date_time_index
+    #ive_df.index = pd.to_datetime(ive_df.index)
+    date_time_index = pd.to_datetime(date_time_index)
+    ive_df.index=date_time_index
+    
+    if verbose>0:
+        display(ive_df.head())
+    if verbose>1:
+        print(ive_df.index)
+        
+    return ive_df
+
+
+
+def load_stock_df_from_csv(filename='ive_sp500_min_data_match_twitter_ts.csv',
+                           folderpath='/content/drive/My Drive/Colab Notebooks/Mod 5 Project/data/',
+                          clean=True,freq='T',fill_method='ffill',verbose=2):
+    import os
+    import pandas as pd
+
+    #         check_for_google_drive()
+        
+    # Check if user provided folderpath to append to filename
+    if len(folderpath)>0:
+        fullfilename = folderpath+filename
+    else:
+        fullfilename=filename
+        
+    # load in csv by fullfilename
+    stock_df = pd.read_csv(fullfilename,index_col=0, parse_dates=True)
+#     stock_df = set_timeindex_freq(stock_df,['BidClose'],freq=freq, fill_method=fill_method)
+    
+    if clean==True:
+        
+        if verbose>0:
+            print(f"Number of 0 values:\n{len(stock_df.loc[stock_df['BidClose']==0])}")
+            print(f"Filling 0 values using method = {fill_method}")
+            
+        stock_df.loc[stock_df['BidClose']==0] = np.nan
+        stock_df['BidClose'].fillna(method=fill_method, inplace=True)
+        
+    
+    # Set the time index 
+    stock_df = set_timeindex_freq(stock_df,'BidClose',freq=freq, fill_method = fill_method, verbose=verbose)
+        
+
+    # Display info depending on verbose level
+    if verbose>0:
+        display(stock_df.head())
+    
+    if verbose>1:
+        print(stock_df.index)
+        
+    return stock_df   
+
+    ######## SEASONAL DECOMPOSITION    
+def plot_decomposition(TS, decomposition, figsize=(12,8),window_used=None):
+    """ Plot the original data and output decomposed components"""
+    
+    # Gather the trend, seasonality and noise of decomposed object
+    trend = decomposition.trend
+    seasonal = decomposition.seasonal
+    residual = decomposition.resid
+
+    fontdict_axlabels = {'fontsize':12}#,'fontweight':'bold'}
+    
+    # Plot gathered statistics
+    fig, ax = plt.subplots(nrows=4, ncols=1,figsize=figsize)
+    
+    ylabel = 'Original'
+    ax[0].plot(np.log(TS), color="blue")
+    ax[0].set_ylabel(ylabel, fontdict=fontdict_axlabels)
+    
+    ylabel = label='Trend'
+    ax[1].plot(trend, color="blue")
+    ax[1].set_ylabel(ylabel, fontdict=fontdict_axlabels)
+    
+    ylabel='Seasonality'
+    ax[2].plot(seasonal, color="blue")
+    ax[2].set_ylabel(ylabel, fontdict=fontdict_axlabels)
+    
+    ylabel='Residuals'
+    ax[3].plot(residual, color="blue")
+    ax[3].set_ylabel(ylabel, fontdict=fontdict_axlabels)
+    ax[3].set_xlabel('Time', fontdict=fontdict_axlabels)
+    
+    # Add title with window 
+    if window_used == None:
+        plt.suptitle('Seasonal Decomposition', y=1.02)
+    else:
+        plt.suptitle(f'Seasonal Decomposition - Window={window_used}', y=1.02)
+    
+    # Adjust aesthetics
+    plt.tight_layout()
+    
+    return ax
+    
+    
+def seasonal_decompose_and_plot(ive_df,col='BidClose',freq='H',
+                          fill_method='ffill',window=144,
+                         model='multiplicative', two_sided=False,
+                               plot_components=True):##WIP:
+    """Perform seasonal_decompose from statsmodels.tsa.seasonal.
+    Plot Output Decomposed Components"""
+    import pandas as pd
+    import numpy as np
+    from statsmodels.tsa.seasonal import seasonal_decompose
+
+
+    # TS = ive_df['BidClose'].asfreq('BH')
+    TS = pd.DataFrame(ive_df[col])
+    TS = TS.asfreq(freq)
+    TS[TS==0]=np.nan
+    TS.fillna(method='ffill',inplace=True)
+
+    # Perform decomposition
+    decomposition = seasonal_decompose(np.log(TS),freq=window, model=model, two_sided=two_sided)
+    
+    if plot_components==True:
+        ax = plot_decomposition(TS, decomposition, window_used=window)
+    
+    return decomposition
