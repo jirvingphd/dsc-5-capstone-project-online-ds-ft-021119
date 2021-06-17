94f8922fef3022d223afdbfd50d16147c12298c5
jirvingphd <james.irving.phd@gmail.com>
2021-06-17 19:36:31 -0400

    made v2021 with bsds package

---
+++ bsds/functions_sklearn.py
@@ -0,0 +1,144 @@
+import datetime as dt
+import time
+import tzlocal as tz
+from sklearn import metrics
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from IPython.display import display
+
+
+def get_time(verbose=False):
+    """Helper function to return current time.
+    Uses tzlocal to display time in local tz, if available."""
+    try: 
+        now =  dt.datetime.now(tz.get_localzone())
+        tic = time.time()
+    except:
+        now = dt.datetime.now()
+        tic = time.time()
+        print("[!] Returning time without tzlocal.")       
+    return now,tic
+        
+    
+def get_report(model,X_test,y_test,as_df=False,label="TEST DATA"):
+    """Get classification report from sklearn and converts to DataFrame"""
+    ## Get Preds and report
+    y_hat_test = model.predict(X_test)
+    scores = metrics.classification_report(y_test, y_hat_test,
+                                          output_dict=as_df) 
+    ## convert to df if as_df
+    if as_df:
+        report = pd.DataFrame(scores).T.round(2)
+        report.iloc[2,[0,1,3]] = ''
+        return report
+    else:
+        header="\tCLASSIFICATION REPORT"
+        if len(label)>0:
+            header += f" - {label}"
+        dashes='---'*20
+        print(f"{dashes}\n{header}\n{dashes}")
+        print(scores)
+        
+        
+        
+    
+def fit_and_time_model(model, X_train,y_train,X_test,y_test,
+                      fit_kws={}, scoring="accuracy",normalize='true',
+                       fmt="%m/%d/%y-%T", verbose=True):
+    """[Fits the provided model and evaluates using provided data.
+
+    Args:
+        model (classifier]): Initialized Model to fit and evaluate
+        X_train (df/matrix): [description]
+        y_train (series/array): [description]
+        X_test (df/matrix): [description]
+        y_test (series/array): [description]
+        fit_kws (dict, optional): Kwargs for .fit. Defaults to {}.
+        scoring (str, optional): Scoring metric to use. Defaults to "accuracy".
+        normalize (str, optional): Normalize confusion matrix. Defaults to 'true'.
+        fmt (str, optional): Time format. Defaults to "%m/%d/%y-%T".
+        verbose (bool, optional): [description]. Defaults to True.
+
+    Raises:
+        Exception: [description]
+    """
+
+    if X_test.ndim==1:
+        raise Exception('The arg order has changed to X_train,y_train,X_test,y_test')
+
+    ## Time
+    start,tic = get_time()
+    if verbose: 
+        print(f"[i] Training started at {start.strftime(fmt)}:")
+        
+    model.fit(X_train, y_train,**fit_kws)
+    
+    ## Calc stop time and elapse
+    stop,toc = get_time()
+    elapsed = toc-tic
+
+
+            
+            
+    ## Get model scores
+    scorer = metrics.get_scorer(scoring)
+    scores_dict ={f'Train':scorer(model,X_train,y_train),  
+                  f'Test':scorer(model, X_test,y_test)}
+    scores_dict['Difference'] = scores_dict['Train'] - scores_dict['Test']
+    scores_df = pd.DataFrame(scores_dict,index=[scoring])
+    
+    ## Time and report back
+    if verbose:
+#         print(f"[i] Training completed at {stop.strftime(fmt)}")
+        if elapsed >120:
+            print(f"\tTraining time was {elapsed/60:.4f} minutes.")
+        else:
+            print(f"\tTraining time was {elapsed:.4f} seconds.")
+    print("\n",scores_df.round(2),"\n")
+    
+    ## Plot Confusion Matrix and display classification report
+    get_report(model,X_test,y_test,as_df=False)
+    
+    fig,ax = plt.subplots(figsize=(10,5),ncols=2)
+    metrics.plot_confusion_matrix(model,X_test,y_test,normalize=normalize,
+                                  cmap='Blues',ax=ax[0])
+
+    try:
+        metrics.plot_roc_curve(model,X_test,y_test,ax=ax[1])
+        ax[1].plot([0,1],[0,1],ls=':')
+        ax[1].grid()
+    except: 
+        fig.delaxes(ax[1])
+    fig.tight_layout()
+    plt.show()
+    return model
+
+
+def evaluate_classification(model, X_test,y_test,normalize='true'):
+    """Plot Confusion Matrix and display classification report"""
+    get_report(model,X_test,y_test,as_df=False)
+    
+    fig,ax = plt.subplots(figsize=(10,5),ncols=2)
+    metrics.plot_confusion_matrix(model,X_test,y_test,normalize=normalize,
+                                  cmap='Blues',ax=ax[0])
+    metrics.plot_roc_curve(model,X_test,y_test,ax=ax[1])
+    ax[1].plot([0,1],[0,1],ls=':')
+    ax[1].grid()
+    fig.tight_layout()
+    plt.show()
+
+
+
+
+def evaluate_grid(grid,X_test,y_test,X_train=None,y_train=None):
+    print('The best parameters were:')
+    print("\t",grid.best_params_)
+    
+    model = grid.best_estimator_    
+
+    print('\n[i] Classification Report')
+    evaluate_classification(model, X_test,y_test)
+    
+    
---
+++ bsds/old_code/functions_temp.py
@@ -0,0 +1,85 @@
+def evaluate_regression_model(model, history, train_generator, test_generator,true_train_series,
+true_test_series,include_train_data=True,return_preds_df = False, save_history=False, history_filename ='results/keras_history.png', save_summary=False, 
+                            summary_filename = 'results/model_summary.txt',auto_unique_filenames=True):
+
+    """Evaluates kera's model's performance, plots model's history,displays classification report,
+    and plots a confusion matrix. 
+    conf_matrix_classes are the labels for the matrix. [negative, positive]
+    Returns df of classification report and fig object for  confusion matrix's plot."""
+
+    from sklearn.metrics import roc_auc_score, roc_curve, classification_report,confusion_matrix
+    # import bs_ds as bs
+    import functions_combined_BEST as ji
+    from IPython.display import display
+    import pandas as pd
+    import matplotlib as mpl
+    numFmt = '.4f'
+    num_dashes = 30
+
+    # results_list=[['Metric','Value']]
+    # metric_list = ['accuracy','precision','recall','f1']
+    print('---'*num_dashes)
+    print('\tTRAINING HISTORY:')
+    print('---'*num_dashes)
+
+    if auto_unique_filenames:
+        ## Get same time suffix for all files
+        time_suffix = ji.auto_filename_time(fname_friendly=True)
+
+        filename_dict= {'history':history_filename,'summary':summary_filename}
+        ## update filenames 
+        for filetype,filename in filename_dict.items():
+            if '.' in filename:
+                filename_dict[filetype] = filename.split('.')[0]+time_suffix + '.'+filename.split('.')[-1]
+            else:
+                if filetype =='summary':
+                    ext='.txt'
+                else:
+                    ext='.png'
+                filename_dict[filetype] = filename+time_suffix + ext
+
+
+        history_filename = filename_dict['history']
+        summary_filename = filename_dict['summary']
+
+
+    ## PLOT HISTORY
+    ji.plot_keras_history( history,filename_base=history_filename, save_fig=save_history,title_text='')
+
+    print('\n')
+    print('---'*num_dashes)
+    print('\tEVALUATE MODEL:')
+    print('---'*num_dashes)
+
+        # # EVALUATE MODEL PREDICTIONS FROM GENERATOR 
+    print('Evaluating Train Generator:')
+    model_metrics_train = model.evaluate_generator(train_generator,verbose=1)
+    print('Evaluating Test Generator:')
+    model_metrics_test = model.evaluate_generator(test_generator,verbose=1)
+
+
+    x_window = test_generator.length
+    n_features = test_generator.data[0].shape[0]
+    gen_df = ji.get_model_preds_from_gen(model=model, test_generator=test_generator,true_test_data=true_test_series,
+        n_input=x_window, n_features=n_features,  suffix='_from_gen',return_df=True)
+
+    regr_results = ji.evaluate_regression_model(y_true=gen_df['true_from_gen'], y_test=gen_df['pred_from_gen'],
+                                metrics=['r2', 'RMSE', 'U'], show_results_df=True, return_as_df=True)
+
+
+    if save_summary:
+        with open(summary_filename,'w') as f:
+            model.summary(print_fn=lambda x: f.write(x+"\n"))
+            f.write(f"\nSaved at {time_suffix}\n")
+
+
+    if include_train_data:
+        true_train_series=true_train_series.rename('true_train_price')
+        df_all_preds=pd.concat([true_train_series,gen_df],axis=1)
+    else:
+        df_all_preds = gen_df
+
+    if return_preds_df:
+        return df_all_preds
+
+
